30bc793953243351d6a0722888ca1e08555dd0a7
Jakub <kubplon@gmail.com>
2020-05-23 11:02:38 +0200

    Commit 4

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println(1);
+        System.out.println("Commit 4");
 
     }
 }
